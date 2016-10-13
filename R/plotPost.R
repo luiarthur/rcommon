6eb76d4b2a652a973de8f73073c5cac7d7cb5f59
Arthur Lui <luiarthur@gmail.com>
2016-10-12 17:33:07 -0700

    yes

--- R/plotPost.R
+++ R/plotPost.R
@@ -55,8 +55,8 @@ plotPost <- function(x,ci=TRUE,stats=TRUE,trace=TRUE,dig=3,cex.ap=1,
   lines(c(xbar,xbar),c(0,bound(xbar,den.x,ret=F)),lwd=2,col="red")
   if (stats) {
     ciString <- ""#paste0("(",round(ci.x[1],dig),", ",round(ci.x[2],dig),")")
-    legend(legend.pos,bty="n",text.col=c(rep("grey20",2),ci.col),cex=cex.l,
-           text.font=c(1,1,2),
+    legend(legend.pos,bty="n",text.col=c("red","grey20",ci.col),cex=cex.l,
+           text.font=c(2,1,2),
            legend=paste(c("Mean: ","SD: ","95% CI "),
                         c(round(xbar,dig),round(sd(x),dig),ciString)))
   }
